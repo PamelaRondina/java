491d97eeb1f32b7d2393e75ccb651dc02eb7ac34
Pamela Rondina <pamela.rondina@gmail.com>
2022-09-22 15:46:50 -0300

    add exercicios

--- java_lets_code/atividades/aula_01.java
+++ java_lets_code/atividades/aula_01.java
@@ -3,20 +3,14 @@
 public class aula_01 {
 
     public static void main(String[] args) {
-        // write your code here
-
-        int a;
-        int b = 2;
-        a = 3;
-        int soma = a + b;
-        int subtracao = a - b;
-        int multiplicacao = a * b;
-        float divisao = (float) a / b;
-
-        System.out.println(soma);
-        System.out.println(subtracao);
-        System.out.println(multiplicacao);
-        System.out.println(divisao);
+        String input = "Let's Code";
+        String name = Optional.ofNullable(input).orElse(() -> getRandomName());
+        System.out.println(name);
+    }
+    private static String getRandomName() {
+        System.out.println("Providing a random name");
+        return "John Doe";
     }
 
 }
+
